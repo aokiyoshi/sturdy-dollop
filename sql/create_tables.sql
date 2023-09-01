47a0d85348bf7ac91658759913cbf2ecc06241c6
Ilshat Kulbaev <87938357+aokiyoshi@users.noreply.github.com>
2023-09-01 17:38:10 +0500

    убраны индексы из файлы создать таблицы

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -13,6 +13,3 @@ CREATE TABLE locations (
     FOREIGN KEY (doctor_id) REFERENCES doctors(id)
 );
 
-CREATE INDEX idx_doctors_id ON doctors (id);
-CREATE INDEX idx_locations_doctor_id ON locations (doctor_id); 
-CREATE INDEX idx_locations_timestamp ON locations (time_stamp);
